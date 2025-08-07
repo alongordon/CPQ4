1857b22cabf06af5c03251bc62dc265a0d9c70f4
alongordon <alon@schematech.co.za>
2025-08-07 13:35:36 +0200

    Add STEP file upload model and admin integration

--- upload_STEP/models.py
+++ upload_STEP/models.py
@@ -1,3 +1,18 @@
+"""Models for the ``upload_STEP`` application."""
+
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
-# Create your models here.
+
+class StepFile(models.Model):
+    """Stores a user uploaded STEP file for later processing."""
+
+    file = models.FileField(
+        upload_to="step_files/",
+        validators=[FileExtensionValidator(["step", "stp"])],
+    )
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:  # pragma: no cover - simple representation
+        return self.file.name
+
